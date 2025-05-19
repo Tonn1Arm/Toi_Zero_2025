b11552423c8e21191195ac9901b7211a2f4ad11e
Tonn1Arm <tonnampuri@gmail.com>
2025-05-20 00:24:04 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,1 @@
-#include <bits/stdc++.h>
-using namespace std;
 
-int main()
-{
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    string str1,str2;
-    cin >> str1 >> str2;
-    cout << "Hello " << str1 << " " << str2 << "\n" << str1.substr(0,2) << str2.substr(0,2);
-    return 0;
-}
